ffae5c8d22365a9eb84f805876c1c3b910889581
Jakob Egger <jakob@eggerapps.at>
2015-09-02 18:55:08 +0200

    Copy handler block (see issue #3)
    Blocks must be copied when they are used after destruction of the scope they were defined in.
    Copyin moves them from the stack to the heap, and retains referenced objects.

--- NSAlert+BlockMethods.m
+++ NSAlert+BlockMethods.m
@@ -12,7 +12,7 @@
 	[self beginSheetModalForWindow: sheetWindow
 					 modalDelegate: self
 					didEndSelector: @selector(blockBasedAlertDidEnd:returnCode:contextInfo:)
-					   contextInfo: (__bridge_retained void*)handler ];
+					   contextInfo: (__bridge_retained void*)[handler copy] ];
 }
 
 - (void) blockBasedAlertDidEnd:(NSAlert *)alert returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo {
